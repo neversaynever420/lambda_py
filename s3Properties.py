fb498c65b6920d217388b71cfd5adb66ad260cde
neversaynever420 <deepak27.v@gmail.com>
2018-09-17 17:01:36 +0530

    Add files via upload

---
+++ s3Properties.py
@@ -0,0 +1,35 @@
+
+import boto3
+from botocore.exceptions import ClientError
+
+s3 = boto3.client('s3')
+# TODO implement
+bucket = 'jiraiya-test-bucket2'
+
+try:
+  data = s3.get_bucket_encryption(Bucket=bucket)
+  if len(data)>0:
+   # print ("Value Present")
+   data = s3.get_bucket_encryption(Bucket=bucket)
+   sseAlgorithm = data['ServerSideEncryptionConfiguration']['Rules'][0]['ApplyServerSideEncryptionByDefault'][
+       'SSEAlgorithm']
+   if sseAlgorithm == 'AES256':
+       # changeEncryption
+       s3.put_bucket_encryption(Bucket=bucket, ServerSideEncryptionConfiguration={'Rules': [
+           {
+               'ApplyServerSideEncryptionByDefault': {
+                   'SSEAlgorithm': 'aws:kms'
+               }
+           },
+       ]})
+
+except ClientError as e:
+  # Bucket has no Default encryption Change it to AWS-KMS
+    # changeEncryption
+    s3.put_bucket_encryption(Bucket=bucket, ServerSideEncryptionConfiguration={'Rules': [
+        {
+            'ApplyServerSideEncryptionByDefault': {
+                'SSEAlgorithm': 'aws:kms'
+            }
+        },
+    ]})
