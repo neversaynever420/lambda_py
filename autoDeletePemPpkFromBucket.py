1537d471b6b3ad139963ac7b7547735c849a1b31
neversaynever420 <deepak27.v@gmail.com>
2018-09-20 10:59:48 +0530

    SNS functionality Added

--- autoDeletePemPpkFromBucket.py
+++ autoDeletePemPpkFromBucket.py
@@ -1,17 +1,18 @@
-import json
 import boto3
-import logging
-
 s3 = boto3.client('s3')
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+sns = boto3.client('sns')
 
 def lambda_handler(event, context):
     # TODO implement
     bucketName = event['detail']['requestParameters']['bucketName']
     objectKey = event['detail']['requestParameters']['key']
-    if objectKey.lower().endswith(('.pem', '.ppk','.jpg')):
+    user = event['detail']['userIdentity']['userName']
+    if objectKey.lower().endswith(('.pem', '.ppk')):
+        sns.publish(
+        TopicArn='arn:aws:sns:ap-south-1:508037994253:s3objectputNotify',
+        Message="%s has uploaded a private key file %s in bucket %s" %(user, objectKey, bucketName),
+        Subject='Illegal filetype upload'
+    )
         response = s3.delete_object(
         Bucket= bucketName,
         Key= objectKey
